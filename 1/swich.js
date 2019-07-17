51d24b19d086838000ad9204c45095b2218275a9
voronov <voronov_r@icloud.com>
2019-07-18 00:19:07 +0300

    project lice in lesson

--- 1/swich.js
+++ 1/swich.js
@@ -6,38 +6,66 @@
 //     console.log("Not working... :(")
 // }
 
-let num = 50
+// let num = 50
 
-//if else else-if
-if (num < 49) {
-    console.log("It's low... :(")
-} else if (num > 100) {
-    console.log("Too much")
-} else {
-    console.log("right")
-}
+// //if else else-if
+// if (num < 49) {
+//     console.log("It's low... :(")
+// } else if (num > 100) {
+//     console.log("Too much")
+// } else {
+//     console.log("right")
+// }
 
 //ternary operator
-(num == 50) ? console.log("right"): console.log("wrong")
+// (num == 50) ? console.log("right"): console.log("wrong")
 
 //switch-case operator
 
-z= +(prompt("input a number"))
-console.log(typeof(z))
-switch (true) {
-    case (z < 49):
-        console.log("wrong");
+// z= +(prompt("input a number"))
+// console.log(typeof(z))
+// switch (true) {
+//     case (z < 49):
+//         console.log("wrong");
+//         break;
+//     case (z > 100):
+//         console.log("too much");
+//         break;
+//     case (z > 80):
+//         console.log("much");
+//         break;
+//     case 50:
+//         console.log("right");
+//         break;
+//     default:
+//         console.log("something goin not right");
+//         break;
+// }
+
+//CICLE
+//let num = 50
+
+// while (num<55){
+//     console.log(num)
+//     num++
+// }
+
+// do {
+//      console.log(num);
+//      num++;
+// }
+// while (num<55);
+
+// for (let i = 1; i<8;i++){
+//     console.log(i)
+// }
+
+for (i=1;i<20;i++){
+    if (i%2==0 || i<8) {
+        continue;
+    }
+    else if (i>14){
         break;
-    case (z > 100):
-        console.log("too much");
-        break;
-    case (z > 80):
-        console.log("much");
-        break;
-    case 50:
-        console.log("right");
-        break;
-    default:
-        console.log("something goin not right");
-        break;
+    }
+    console.log(i)
 }
--- proj/script.js
+++ proj/script.js
@@ -1,21 +1,24 @@
 'use strict'
 
-let money = prompt("You budget on the month?");
-let time = prompt("Input date in format YYYY-MM-DD");
+let money = prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let first = prompt("Enter mandatory expenses item in this month");
 let second = prompt("How much it will cost?")
 let appData = {
     budget: money,
     timeData: time,
-    expenses: {
-        first: second
-    },
-    optionalExpenses: {
-
-    },
+    expenses: {},
+    optionalExpenses: {},
     income: [],
     saving: false
 }
+let a1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a2 = prompt("Во сколько обойдется?"),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a4 = prompt("Во сколько обойдется?");
 
-alert("You budget on 1 day "+ money/30)
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert("You budget on 1 day "+ appData.budget/30);
