3e1bc11ef747a28003ef9769690bad2a4272387b
voronov <voronov_r@icloud.com>
2019-12-16 14:07:09 +0200

    very much work after big pause

--- 1/script.js
+++ 1/script.js
@@ -8,27 +8,29 @@
 
 //*********CALLBACK FUNC****
 
-function first(){
-    //do something
-    setTimeout( function(){
-        console.log(1);
-    }, 500);
-}
-function second(){
-    console.log(2)
-}
+// function first(){
+//     //do something
+//     setTimeout( function(){
+//         console.log(1);
+//     }, 500);
+// }
+// function second(){
+//     console.log(2)
+// }
 
-//first();
-//second();
+// first();
+// second();
 
-function learnJS(lang, callback){
-    console.log("im study "+lang);
-    callback();
-}
+// function learnJS(lang, callback){
+//     console.log("im study "+lang);
+//     callback(); 
+// }
 
-learnJS("javascript", function(){
-   console.log("im ended 3td lesson")
-})
+// learnJS("javascript", function(){
+//    console.log("i'm ended a 3td lesson")
+// })
+
+
 // function some_function(arg1, arg2, callback) {
 //     // переменная, генерирующая случайное число в интервале между arg1 и arg2
 //     var my_number = Math.ceil(Math.random() * (arg1 - arg2) + arg2);
@@ -42,32 +44,142 @@ learnJS("javascript", function(){
 //     console.log("callback called! " + num);
 // });
 
-function some_function2(url, callback) {
-    var httpRequest; // создаём наш XMLHttpRequest-объект
-    if (window.XMLHttpRequest) {
-        httpRequest = new XMLHttpRequest();
-    } else if (window.ActiveXObject) {
-        // для дурацкого Internet Explorer'а
-        httpRequest = new
-        ActiveXObject("Microsoft.XMLHTTP");
-    }
-    httpRequest.onreadystatechange = function () {
-        // встраиваем функцию проверки статуса нашего запроса
-        // это вызывается при каждом изменении статуса
-        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
-            callback.call(httpRequest.responseXML); // вызываем колбек
-        }
-    };
-    httpRequest.open('GET', url);
-    httpRequest.send();
+// function some_function2(url, callback) {
+//     var httpRequest; // создаём наш XMLHttpRequest-объект
+//     if (window.XMLHttpRequest) {
+//         httpRequest = new XMLHttpRequest();
+//     } else if (window.ActiveXObject) {
+//         // для дурацкого Internet Explorer'а
+//         httpRequest = new
+//         ActiveXObject("Microsoft.XMLHTTP");
+//     }
+//     httpRequest.onreadystatechange = function () {
+//         // встраиваем функцию проверки статуса нашего запроса
+//         // это вызывается при каждом изменении статуса
+//         if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+//             callback.call(httpRequest.responseXML); // вызываем колбек
+//         }
+//     };
+//     httpRequest.open('GET', url);
+//     httpRequest.send();
+// }
+// // вызываем функцию
+// some_function2("text.xml", function () {
+//     console.log(this);
+// });
+// console.log("это выполнится до вышеуказанного колбека");
+
+// //****************/Start object training
+// let options = {
+//     width: 1024,
+//     height: 1024,
+//     name: "test"
+// };
+// console.log(options.name);
+
+// //training add to object
+// options.bool = false;
+// options.colors = {
+//     border: "black",
+//     background: "red",
+// }
+// //delete training from obj 
+// delete options.bool;
+
+// console.log(options);
+
+// //training loop in obj
+// for (let  key in options) {
+//     console.log("Свойство " +key+ " имеет значение " + options[key]);
+// }
+// //узнаем количество свойст в обьекте
+// console.log(Object.keys(options).length);
+// //*******************************
+
+// ******************** ARRAY TRAINING
+// let arr = [1,2,3,4,5];
+
+// arr.pop();             //delete from end arr
+// arr.push("nine")       //add to end of arr
+
+// arr.shift()
+// arr.unshift("five")
+
+// // for (let i=0; i<arr.length; i++){
+// //     console.log(arr[i]);
+// // };
+
+// arr.forEach(function(item, i, mass){
+//     console.log(i+": "+ item+" (массив: " +mass+ ")")
+// });
+
+// console.log(arr)
+
+// let aarr = [1,2,3,4,7];
+// for(let key in aarr){
+//     console.log(key);
+// };
+// for(let key of aarr){
+//     console.log(key);
+// };
+
+
+// let ans = prompt("", ""),
+//     arr = [];
+// arr = ans.split(",");
+//  console.log(arr);
+
+
+// let arr = ["awdada", "awdad", "zzzz"],
+//     i = arr.join(", ");
+// console.log(i);
+
+
+// let arr = ["awdada", "xxwdad", "bs zzz"],
+//     i = arr.sort();
+// console.log(arr);
+
+
+// let arr = [1,15,6],
+//     i = arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a,b){
+//     return a-b;
+// }
+// //****************************************************
+
+// //*****************************OOP TRAINING
+// let soldier = {
+//     health: 400,
+//     armor: 100
+// };
+
+// let john = {
+//     health: 100
+// };
+
+// john.__proto__ = soldier;
+
+// console.log(john);
+// console.log(john.armor );
+// //*************************************
+
+
+function hello(){
+    console.log("hello world!");
 }
-// вызываем функцию
-some_function2("text.xml", function () {
-    console.log(this);
-});
-console.log("это выполнится до вышеуказанного колбека");
+hello();
 
+function hi(){
+    console.log("hi world!");
+}
+hi();
 
+let arr = [1, 15, 4, 30, 42],
+    i=arr.sort(compareNum);
 
-
-
+function compareNum(a, b){
+    return a-b;
+}
+console.log(arr);
--- proj/script.js
+++ proj/script.js
@@ -5,72 +5,92 @@ function start() {
     money = +prompt("Ваш бюджет на месяц?", "");
     time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
-    while(isNaN(money) || money=="" || money == null ){
+    while(isNaN(money) || money== "" || money == null ){
         money = +prompt("Ваш бюджет на месяц?", "");
     }
 }
-start();
+//start();
 
 
 let appData = {
     budget: money,
-    timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
+    timeData: time,
     saving: true,
-}
-
-function chooseExpenses(){
-    for(let i=0; i<2; i++){
-        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-            b = prompt("Во сколько обойдется?","");
-        //Проверяем ввод
-        if( (typeof(a))==="string" && typeof(a) != null && (typeof(b)) != null
-            && a != "" && b!="" && a.length<50){
-            console.log("done"+ i)
-            appData.expenses[a] = b;
-        }else{
-            i = i-1;
-        }   
-    };
-}
-chooseExpenses();
-
-function detectDayBudget(){
-    appData.moneyPerDay = (appData.budget/30).toFixed();
-    alert("You budget on 1 day "+ appData.moneyPerDay);
-}
-//detectDayBudget();
-
-function detectRichLevel(){
-    if(appData.moneyPerDay<100){
-        console.log("minimal level rich");
-    }else if(appData.moneyPerDay>100 && appData.moneyPerDay<2000){
-        console.log("middle level rich");
-    }else if(appData.moneyPerDay>2000){
-        console.log("high level rich");
-    } else {
-        console.log("Error");
-    }
-}
-detectRichLevel();
-
-
-function checkSavings(){
-    if ((appData.saving) == true){
-        let save = +prompt("How much money you saving?"),
-            persent = +prompt("What procent?");
+    chooseExpenses: function(){
+        for(let i=0; i<2; i++){
+            let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+                b = prompt("Во сколько обойдется?","");
+            //Проверяем ввод
+            if( (typeof(a))==="string" && typeof(a) != null && (typeof(b)) != null
+                && a != "" && b!="" && a.length<50){
+                console.log("done"+ i)
+                appData.expenses[a] = b;
+            }else{
+                i = i-1;
+            }   
+        }
+    },
+    detectDayBudget: function(){
+        appData.moneyPerDay = (appData.budget/30).toFixed();
+        alert("Ежедневный бюджет: "+ appData.moneyPerDay);
+    },
+    detectRichLevel: function(){
+        if(appData.moneyPerDay<100){
+            console.log("Мининмальный уровень достатка");
+        }else if(appData.moneyPerDay>100 && appData.moneyPerDay<2000){
+            console.log("Средний уровень достатка");
+        }else if(appData.moneyPerDay>2000){
+            console.log("Высокий уровень достатка");
+        } else {
+            console.log("Произошла ошибка");
+        }  
+    },
+    checkSavings: function(){
+        if ((appData.saving) == true){
+            let save = +prompt("Какова сумма накоплений?"),
+                persent = +prompt("Под какой процент?");
+            
+            appData.monthIncome = save/100/12*persent;
+            alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+        }        
+    },
+    chooseOptionalExpenses: function(){
+        for (let i=0; i<3; i++){
+            appData.optionalExpenses[i+1]=prompt("Input optional Expenses");
+        }        
+    },
+    chooseIncome: function(){
+        let items = prompt("Что принесет дополнительный доход? (Перечислите через зарятую)", "");
         
-        appData.monthIncome = save/100/12*persent;
-        alert("Profit from you depozit is: " + appData.monthIncome);
-    }
-}
-checkSavings();
+        while (items == null || items=="" || isNaN(items)==false) {
+            items = prompt("Что принесет дополнительный доход? (Перечислите через зарятую)", "");
+        }
 
-function chooseOptionalExpenses(){
-    for (let i=0; i<3; i++){
-        appData.optionalExpenses[i+1]=prompt("Input optional Expenses");
-    }
+        appData.income = items.split(", ");
+        
+        appData.income.push(prompt("Может что-то еще?"));
+        appData.income.sort();
+
+        
+        this.income.forEach(function(items, i) {
+            console.log("Способы доп заработка: " + (i+1) +" "+ items)
+        });
+        //console.log(appData.income)
+
+    },
+};
+
+//appData.chooseExpenses();
+//appData.detectDayBudget();
+//appData.detectRichLevel();
+//appData.checkSavings();
+//appData.chooseOptionalExpenses();
+//appData.chooseIncome();
+
+
+for (let key in appData){
+    console.log("Наша программа включает в себя данные:"+ key)
 }
-chooseOptionalExpenses();
