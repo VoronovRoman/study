35ae96697b1be11a6d6d87463a1e47f00f6c1eda
voronov <voronov_r@icloud.com>
2020-01-21 17:26:02 +0200

    calc to yoga proj end

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -372,4 +372,31 @@ window.addEventListener("DOMContentLoaded", function () {
         }
     });
     //SLIDER FROM GUIDE END
+
+    //CALC START
+    let total = document.querySelector(".counter #total"),
+        counterInputs = document.querySelectorAll(".counter input"),
+        restPlaces = document.querySelector(".counter select"),
+        sum = 0; 
+    
+    total.innerHTML= "0"
+    counterInputs.forEach((elem)=>{
+        elem.addEventListener("input", (event)=>{
+            if(counterInputs[0].value == "" || counterInputs[1].value == ""){
+                total.innerHTML = "0"
+            }else{
+                sum = (+counterInputs[0].value + (+counterInputs[1].value))*100
+                total.innerHTML = sum
+            }
+            //set select option field to default :)
+            restPlaces.value = "1"
+        })
+    })
+    restPlaces.addEventListener("change", (e)=>{
+        let a = sum
+        total.innerHTML = a*e.target.value
+    })
+    //CALC END
+
+
 });
